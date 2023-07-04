d9258807022ba2b505ef4a2de0bc89c4cba575b3
Hiroya Taguchi <freigabe1031macquarie@gmail.com>
2023-07-04 12:23:59 +0000

    test6 for pytest

--- taskman/main.py
+++ taskman/main.py
@@ -7,16 +7,16 @@ from typing_extensions import Annotated
 from fastapi import Depends, FastAPI
 from starlette.responses import RedirectResponse
 
-from opentelemetry import trace
-from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import (
-    BatchSpanProcessor,
-    # ConsoleSpanExporter,
-)
-# from opentelemetry.trace import Link
+# # from opentelemetry import trace
+# from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter
+# from opentelemetry.sdk.trace import TracerProvider
+# from opentelemetry.sdk.trace.export import (
+#     BatchSpanProcessor,
+#     # ConsoleSpanExporter,
+# )
+# # from opentelemetry.trace import Link
 
-from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+# from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 
 from .backends import Backend, RedisBackend, MemoryBackend, GCSBackend
 from .model import Task, TaskRequest
@@ -58,8 +58,8 @@ def get_tasks(backend: Annotated[Backend, Depends(get_backend)]) -> List[Task]:
 def get_task(task_id: str,
              backend: Annotated[Backend, Depends(get_backend)]) -> Task:
     # with tracer.start_as_current_span("get_task"):
-        return backend.get(task_id)
-
+        # return backend.get(task_id)
+    return backend.get(task_id)
 
 
 @app.put('/tasks/{item_id}')
